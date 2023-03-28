783b933d12f388eff457903468bc91c10aec5f83
elena.lazareva <eknyle@yandex.ru>
2023-03-28 10:43:08 +0300

    fix:another fix

--- routes/cards.js
+++ routes/cards.js
@@ -14,7 +14,7 @@ router.get('/', auth, getCards);
 router.post('/', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(/https?\:\/\/(www\.)?[\w\-\.\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]{1,256}\.[a-zA-Zа-яА-Я]{1,6}#?/gi),
+    link: Joi.string().required().pattern(/https?\:\/\/(www\.)?[\w\-\.\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]{1,256}\.[a-zA-Zа-яА-Я]{1,6}#?/,Joi.boolean()),
   }),
 }), createCard);
 router.delete('/:cardId', auth, celebrate({
